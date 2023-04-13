9afad224896abda18b96d3d1247bcb8505f60afb
Lucas Paskua <80064601+paskua4@users.noreply.github.com>
2023-04-13 19:52:20 -0300

    tentativa de fazer react reveal funcionar

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/lucaspaschoalick.github.io.git/' : '',
+  assetPrefix: isProd ? '' : '',
   images: {
     unoptimized: true,
   },
